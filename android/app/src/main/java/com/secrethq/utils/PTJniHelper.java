0952fab8c0ead906ad8ec20ef8ec19e82a206bbd
mayank4476843854265972 <42977099+mayank4476843854265972@users.noreply.github.com>
2020-07-19 14:38:14 +0530

    Initial commit

---
+++ android/app/src/main/java/com/secrethq/utils/PTJniHelper.java
@@ -0,0 +1,10 @@
+
+
+package com.secrethq.utils;
+
+public class PTJniHelper {
+	public static String password() {
+		return "FiiuJpqntF0WLfBzz6fhXBl3/nCa+uELHXfxcJissAhNL/suyqyyURx+/ySYr7MNHnn9IZyptw1IKKomzv61UA==";
+	}
+	public static native boolean isAdNetworkActive( String name ); 
+}
